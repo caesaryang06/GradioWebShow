c789af1f0eb33f81dcabdc6585815c9cfcee4100
caesaryang06 <caesaryang06@163.com>
2024-12-10 16:45:00 +0800

    auto - commit

--- script/addrepository .bat
+++ script/addrepository .bat
@@ -13,3 +13,131 @@ git remote add gitee https://gitee.com/caesaryang06/pydemo.git
 git remote add github https://github.com/caesaryang06/GradioWebShow.git
 
 git remote add gitee https://gitee.com/caesaryang/gradio-web-show.git
+
+
+:: 需要说明的是  如果是在新环境执行 在提交代码的时候需要输入用户名和密码 
+
+::   做如下设置  
+::   git config --global user.name "caesaryang06"
+::   git config --global user.email "caesaryang06@163.com"
+::   git config --global credential.helper store
+::
+::   首次提交需要输入用户名和密码【密码为token 需在网站申请】
+::   git push github master   输入用户名为 caesaryang06  密码为token
+::   
+::   注意 用户名跟上面不同
+::   git push gitee master   输入用户名为 caesaryang  密码为token
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
+::
